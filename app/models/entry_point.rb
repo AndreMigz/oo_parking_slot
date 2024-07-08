58902f5bebe25d491ca406def714bd12853742ba
AndreMigz <migzarre@gmail.com>
2024-07-08 12:09:42 +0800

    Add EntryPoint Callback for parking slot creation
    Description:
    -  This commit adds a callback to create new parking slots record whenever a new entry point is being added.

--- app/models/entry_point.rb
+++ app/models/entry_point.rb
@@ -1,4 +1,23 @@
 class EntryPoint < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness:  { :message => "This name is already taken." }
+
+  after_create :create_parking_slots
+
+  private
+
+  def create_parking_slots
+    [
+      { size: 'sp', distances: { id.to_s => 1 }.to_json, occupied: false },
+      { size: 'mp', distances: { id.to_s => 2 }.to_json, occupied: false },
+      { size: 'lp', distances: { id.to_s => 3 }.to_json, occupied: false }
+    ].each_with_index do |slot_params, index|
+      ParkingSlot.create(
+        size: slot_params[:size],
+        distances: slot_params[:distances],
+        occupied: slot_params[:occupied],
+        name: "#{name}#{index + 1}"
+      )
+    end
+  end
 end
--- test/controllers/entry_points_controller_test.rb
+++ test/controllers/entry_points_controller_test.rb
@@ -9,13 +9,15 @@ class EntryPointsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should be able to create entry points using the create route' do
     assert_difference('EntryPoint.count') do
-      post entry_points_path, params:
-      {
-        entry_point:
+      assert_difference('ParkingSlot.count', +3) do # Creating an entry point should trigger the callback to create 3 new parking slots
+        post entry_points_path, params:
         {
-          name: 'Test Entry Point'
+          entry_point:
+          {
+            name: 'Test Entry Point'
+          }
         }
-      }
+      end
     end
 
     assert_response :created
